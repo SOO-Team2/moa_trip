521f6eae5d755e2d110b10f6f23ffc7fab87e9ef
mjjun9 <mjjun9@gmail.com>
2026-09-14 10:43:41 +0900

    feat: html 파일 통합 및 정리

--- moa_trip_app/urls.py
+++ moa_trip_app/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.main, name='main'),
     path('explore/', views.explore, name='explore'),
+    path('detail/', views.detail, name='detail'),
+    path('planner/', views.planner, name='planner'),
     path('mypage/', views.mypage, name='mypage'),
+    path('login/', views.login, name='login'),
+    path('moa_admin/', views.admin, name='admin'),
 ]
