ffd6b22d2062028fbcc65e76dea35182f839f823
mjjun9 <mjjun9@gmail.com>
2026-09-15 10:37:32 +0900

    feat: 인증키 파일 추가 및 gitignore 수정

--- moa_trip/settings.py
+++ moa_trip/settings.py
@@ -130,6 +130,3 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
-
-# 공공데이터 API
-KMA_API_KEY = 'a2QRrEzCmxvn5HHIDRG8ql2FCynA0kAFDsse7JJ50NrjBn%2BTmI5iunhQoNnNeUJElublkgv8FcYckdN8hYsh8g%3D%3D'
