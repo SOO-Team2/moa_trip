da63f92bd2c97b87722e322dce2db46bfcab6f7d
mjjun9 <mjjun9@gmail.com>
2026-09-18 15:08:56 +0900

    fix: 탐색 페이지 반려동물 필터 하나 삭제

--- moa_trip/settings.py
+++ moa_trip/settings.py
@@ -20,7 +20,6 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
